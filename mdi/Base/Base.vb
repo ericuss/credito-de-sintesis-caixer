67afd2fa1022aded1bd74d4f138a57f4d47793dd
rboix90@gmail.com <rboix90@gmail.com@755e9518-00f4-6748-72d5-f8c0b8ac8041>
2012-05-22 17:06:10 +0000

    add comments - regions

--- mdi/Base/Base.vb
+++ mdi/Base/Base.vb
@@ -1,12 +1,43 @@
-﻿
+﻿''' <summary>
+''' Clase utilizada para ser heredada en los demas formularios.
+''' </summary>
 Public Class Base
-
+#Region "Propiedades"
+    ''' <summary>
+    ''' Propiedad para indicar a que tabla de la BBDD se va a conectar
+    ''' </summary>
     Protected Friend tablaBBDD As String
-    Protected Friend dts As DataSet
-    Public strTitulo As String = ""
-    Protected Friend strOpcional As String
-    Protected Friend strQuery As String = ""
 
+    ''' <summary>
+    ''' Propiedad para guardar los datos de la DataGridView
+    ''' </summary>
+    Protected Friend dts As DataSet
+
+    ''' <summary>
+    ''' Titulo para la impresion del PDF
+    ''' </summary>
+    Public strTitulo As String = ""
+
+    ''' <summary>
+    ''' Propiedad para indicar datos adicionales al PDF
+    ''' </summary>
+    ''' <remarks></remarks>
+    Protected Friend strOpcional As String
+
+    ''' <summary>
+    ''' Propiedad para sobreescrivir la consulta de sql para rellenar la DataGridView
+    ''' </summary>
+    ''' <remarks></remarks>
+    Protected Friend strQuery As String = ""
+#End Region
+
+#Region "Eventes"
+
+    ''' <summary>
+    ''' Evento que carga cuando se muestra el Formulario, qui cargamos la DataGridView vacia.
+    ''' </summary>
+    ''' <param name="sender">Objeto del evento</param>
+    ''' <param name="e">Objeto del evento</param>
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
         Dim conn As New AccDatos.OLEDBCON
@@ -20,24 +51,21 @@ Public Class Base
         ocultarId()
         KeyPreview = True
     End Sub
-    Public Overridable Sub ocultarId()
 
-    End Sub
-    Public Overridable Sub filtrarGrid()
-
-    End Sub
-
-    Protected Friend Sub loadfirst()
-        Dim conn As New AccDatos.OLEDBCON
-        dts = conn.ObtenerTablaVacia(tablaBBDD)
-        ocultarId()
-        bindearDTS()
-    End Sub
+    ''' <summary>
+    ''' Evento que cierra el formulario.
+    ''' </summary>
+    ''' <param name="sender">Objeto del evento</param>
+    ''' <param name="e">Objeto del evento</param>
     Private Sub txtSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtSalir.Click
         Me.Dispose()
     End Sub
 
-
+    ''' <summary>
+    ''' Evento que ejecuta un update de los cambios realizados en la DataGridView
+    ''' </summary>
+    ''' <param name="sender">Objeto del evento</param>
+    ''' <param name="e">Objeto del evento</param>
     Private Sub btnUpdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpdate.Click
         Dim conn As New AccDatos.OLEDBCON
         If strQuery = "" Then
@@ -47,8 +75,11 @@ Public Class Base
         End If
     End Sub
 
-
-
+    ''' <summary>
+    ''' Metodo que obtiene todos los datos de la tabla indicada
+    ''' </summary>
+    ''' <param name="sender">Objeto del evento</param>
+    ''' <param name="e">Objeto del evento</param>
     Private Sub btnTodos_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTodos.Click
         Dim conn As New AccDatos.OLEDBCON
         If strQuery = "" Then
@@ -59,6 +90,94 @@ Public Class Base
         limpiarBinding()
         bindearDTS()
         ocultarId()
+    End Sub
+
+    ''' <summary>
+    ''' Metodo que limpia la DataGridView, lanzando una consulta nueva, obteniendo solo el nombre de los campos
+    ''' </summary>
+    ''' <param name="sender">Objeto del evento</param>
+    ''' <param name="e">Objeto del evento</param>
+    Private Sub btnLimpiar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLimpiar.Click
+        limpiarBinding()
+        Dim conn As New AccDatos.OLEDBCON
+        dts = conn.ObtenerTablaVacia(tablaBBDD)
+        bindearDTS()
+        ocultarId()
+    End Sub
+
+    ''' <summary>
+    ''' Metodo que genera el pdf a partir de la DataGridView
+    ''' </summary>
+    ''' <param name="sender">Objeto del evento</param>
+    ''' <param name="e">Objeto del evento</param>
+    Private Sub btnPdf_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPdf.Click
+        tools.clsTools.imprimirDataTableEnPdf(dts, strTitulo, strOpcional)
+    End Sub
+
+    ''' <summary>
+    ''' Evento que al pulsar la tecla F3 filtra los resultados de la DaaGridView
+    ''' </summary>
+    ''' <param name="sender">Objeto del evento</param>
+    ''' <param name="e">Objeto del evento</param>
+    Private Sub Base_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyCode = 114 Then
+            filtrarGrid()
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Evento que limpia los filtros
+    ''' </summary>
+    ''' <param name="sender">Objeto del evento</param>
+    ''' <param name="e">Objeto del evento</param>
+    Public Sub btnLimpiarControles_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLimpiarControles.Click
+        Try
+
+            For Each txt In Me.Controls
+
+                If txt.GetType().ToString() = "System.Windows.Forms.GroupBox" Then
+
+                    For Each objTxt In txt.controls
+                        If objTxt.GetType().ToString() = "CustomValidatorTextBox.CustomValidatorTextBox" Or objTxt.GetType().ToString() = "System.Windows.Forms.TextBox" Then
+                            objTxt.text = ""
+                        End If
+
+                        If objTxt.GetType().ToString() = "customTextCs.txtBuscar" Then
+                            objTxt.zzTxtId = ""
+                            objTxt.zzTxtDesc = ""
+                        End If
+
+                    Next
+
+                ElseIf txt.GetType().ToString() = "CustomValidatorTextBox.CustomValidatorTextBox" Or txt.GetType().ToString() = "System.Windows.Forms.TextBox" Then
+                    txt.text = ""
+                ElseIf txt.GetType().ToString() = "customTextCs.txtBuscar" Then
+                    txt.zzTxtId = ""
+                    txt.zzTxtDesc = ""
+                End If
+            Next
+            Dim conn As New AccDatos.OLEDBCON
+            If strQuery = "" Then
+                dts = conn.ObtenerTabla(tablaBBDD)
+            Else
+                dts = conn.LanzarQueryT(strQuery)
+            End If
+            limpiarBinding()
+            bindearDTS()
+            ocultarId()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+#End Region
+
+#Region "Metodos"
+    Protected Friend Sub loadfirst()
+        Dim conn As New AccDatos.OLEDBCON
+        dts = conn.ObtenerTablaVacia(tablaBBDD)
+        ocultarId()
+        bindearDTS()
     End Sub
 
     Private Sub bindearDTS()
@@ -77,6 +196,7 @@ Public Class Base
             'MsgBox(ex.Message.ToString)
         End Try
     End Sub
+
     Private Sub validartext(ByVal sender As Object, ByVal e As System.EventArgs)
         DirectCast(sender, Windows.Forms.Control).DataBindings(0) _
         .BindingManagerBase.EndCurrentEdit()
@@ -97,65 +217,23 @@ Public Class Base
             MsgBox(ex.Message.ToString)
         End Try
     End Sub
+#End Region
 
-    Private Sub btnLimpiar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLimpiar.Click
-
-        limpiarBinding()
-        Dim conn As New AccDatos.OLEDBCON
-        dts = conn.ObtenerTablaVacia(tablaBBDD)
-        bindearDTS()
-        ocultarId()
-    End Sub
-
-    Private Sub btnPdf_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPdf.Click
-
-        tools.clsTools.imprimirDataTableEnPdf(dts, strTitulo, strOpcional)
-    End Sub
-
-    Private Sub Base_KeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyUp
-        If e.KeyCode = 114 Then
-            filtrarGrid()
-        End If
+#Region "Overriable"
+    ''' <summary>
+    ''' Metodo sobreescrivible que oculta las IDs de la DataGridView
+    ''' </summary>
+    Public Overridable Sub ocultarId()
 
     End Sub
 
-    Public Sub btnLimpiarControles_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLimpiarControles.Click
-        Try
+    ''' <summary>
+    ''' Metodo sobreescrivible que filtra los resultados de la DataGridView
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Overridable Sub filtrarGrid()
 
-            For Each txt In Me.Controls
-
-                If txt.GetType().ToString() = "System.Windows.Forms.GroupBox" Then
-
-                    For Each objTxt In txt.controls
-                        If objTxt.GetType().ToString() = "CustomValidatorTextBox.CustomValidatorTextBox" Or objTxt.GetType().ToString() = "System.Windows.Forms.TextBox" Then
-                            objTxt.text = ""
-                        End If
-
-                        If objTxt.GetType().ToString() = "customTextCs.txtBuscar" Then
-                            objTxt.zzTxtId = ""
-                            objTxt.zzTxtDesc = ""
-                        End If
-
-            Next
-
-                ElseIf txt.GetType().ToString() = "CustomValidatorTextBox.CustomValidatorTextBox" Or txt.GetType().ToString() = "System.Windows.Forms.TextBox" Then
-                    txt.text = ""
-                ElseIf txt.GetType().ToString() = "customTextCs.txtBuscar" Then
-                    txt.zzTxtId = ""
-                    txt.zzTxtDesc = ""
-                    End If
-            Next
-            Dim conn As New AccDatos.OLEDBCON
-            If strQuery = "" Then
-                dts = conn.ObtenerTabla(tablaBBDD)
-            Else
-                dts = conn.LanzarQueryT(strQuery)
-            End If
-            limpiarBinding()
-            bindearDTS()
-            ocultarId()
-        Catch ex As Exception
-
-        End Try
     End Sub
+#End Region
+
 End Class
